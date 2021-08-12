61f2989a5b6735bcda5c4904aaf284acbbf50f9c
Steffen Kötte <steffen.koette@gmail.com>
2021-08-11 22:53:38 -0700

    Remove example test code

--- Tests/RogersBankDownloaderTests/RogersBankDownloaderTests.swift
+++ Tests/RogersBankDownloaderTests/RogersBankDownloaderTests.swift
@@ -2,10 +2,5 @@
 import XCTest
 
 final class RogersBankDownloaderTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RogersBankDownloader().text, "Hello, World!")
-    }
+
 }
