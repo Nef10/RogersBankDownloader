3e57732a5f28391f6fc5fa9811977b54e6d03590
Steffen Kötte <steffen.koette@gmail.com>
2024-11-06 23:15:25 -0800

    Handle two factor authentication
    fixes #4

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 
 ## What
 
-This is a small library to download transaction data for Rogers Bank Credit Cards. To authenticate it currently uses requires the `deviceId` and `deviceInfo` from a trusted device to skip the 2FA.
+This is a small library to download transaction data for Rogers Bank Credit Cards.
 
 ## How
 
-1) Obtain a deviceId with the matching deviceInfo by logging into the website and having a look at the network tab in the developer tools
-2) Call `User.load(username: "x", password: "x,", deviceId: "x", deviceInfo: "x")`
-3) In the completion handler check that the login was successful: `if case let .success(user) = $0 {`
+1) Create an instance of `RogersAuthenticator` and set your delegate.
+2) Call `login(username: "x", password: "x,", deviceId: "x")` on it. If you don't have a deviceId, pass in nil, otherwise pass the one you received from the delegate after the last successful login.
+3) In the completion handler check that the login was successful: `if case let .success(user) = $0`
 4) `user` now contains the logged in user, which contains the accounts - these already contain info about the current balance, last statement amount, among other information
 5) If you want to download the transactions, e.g. call `user.accounts[0].downloadActivities(statementNumber: 0)` - if you have more than one account change the index accordingly - the `statementNumber` parameter indicates which statement period to download the transactions from, with 0 meaning the current period, 1 the last statement and so on.
 
--- Sources/RogersBankDownloader/RogersUser.swift
+++ Sources/RogersBankDownloader/RogersUser.swift
@@ -1,7 +1,4 @@
 import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
 
 /// An online user account
 public protocol User {
@@ -13,17 +10,9 @@ public protocol User {
     /// Authenticated
     var authenticated: Bool { get }
 
-    /// Login and load a user account
-    /// - Parameters:
-    ///   - username: user name
-    ///   - password: password
-    ///   - deviceId: device id to skip 2FA
-    ///   - deviceInfo: device info to skip 2FA
-    ///   - completion: completion handler, returns the User or a Download Error
-    static func load(username: String, password: String, deviceId: String, deviceInfo: String, completion: @escaping (Result<User, DownloadError>) -> Void)
-
 }
 
+/// A rogers online user account
 public struct RogersUser: User, Codable {
 
     enum CodingKeys: String, CodingKey {
@@ -31,26 +20,6 @@ public struct RogersUser: User, Codable {
         case authenticated
         case rogersAccounts = "accounts"
     }
-
-    private static var startURL = URL(string: "https://rbaccess.rogersbank.com/?product=ROGERSBRAND")!
-    private static var authenticationURL = URL(string: "https://rbaccess.rogersbank.com/issuing/digital/authenticate/user")!
-
-    private static let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        return dateFormatter
-    }()
-
-    private static let authenticationRequest: URLRequest = {
-        var request = URLRequest(url: authenticationURL)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("ROGERSBRAND", forHTTPHeaderField: "Brand_id")
-        request.setValue("json", forHTTPHeaderField: "Datatype")
-        request.setValue("web", forHTTPHeaderField: "Sourcetype")
-        return request
-    }()
 
     public let userName: String
     public let authenticated: Bool
@@ -60,77 +29,4 @@ public struct RogersUser: User, Codable {
         rogersAccounts
     }
 
-    public static func load(username: String, password: String, deviceId: String, deviceInfo: String, completion: @escaping (Result<User, DownloadError>) -> Void) {
-        sendStartRequest {
-            if let error = $0 {
-                completion(.failure(error))
-                return
-            }
-            let parameters = [
-                "username": username,
-                "password": password,
-                "deviceId": deviceId,
-                "deviceInfo": deviceInfo
-            ]
-            sendAuthenticationRequest(parameters: parameters) {
-                completion($0)
-            }
-        }
-    }
-
-    private static func sendStartRequest(completion: @escaping (DownloadError?) -> Void) {
-        var request = URLRequest(url: startURL)
-        request.httpMethod = "GET"
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard data != nil else {
-                if let error {
-                    completion(DownloadError.httpError(error: error.localizedDescription))
-                } else {
-                    completion(DownloadError.noDataReceived)
-                }
-                return
-            }
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(DownloadError.httpError(error: "No HTTPURLResponse"))
-                return
-            }
-            guard httpResponse.statusCode == 200 else {
-                completion(DownloadError.httpError(error: "Status code \(httpResponse.statusCode)"))
-                return
-            }
-            completion(nil)
-        }
-        task.resume()
-    }
-
-    private static func sendAuthenticationRequest(parameters: [String: String], completion: @escaping (Result<User, DownloadError>) -> Void) {
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-            completion(.failure(DownloadError.invalidParameters(parameters: parameters)))
-            return
-        }
-
-        let task = URLSession.shared.uploadTask(with: authenticationRequest, from: jsonData) { data, response, error in
-            let processedResponse = URLTaskHelper.processResponse(data: data, response: response, error: error)
-            guard case let .success((data, httpResponse)) = processedResponse else {
-                if case let .failure(error) = processedResponse {
-                    completion(.failure(error))
-                }
-                return
-            }
-            guard httpResponse.statusCode == 200 else {
-                completion(.failure(DownloadError.httpError(error: "Status code \(httpResponse.statusCode)")))
-                return
-            }
-            do {
-                let decoder = JSONDecoder()
-                decoder.dateDecodingStrategy = .formatted(dateFormatter)
-                let user = try decoder.decode(Self.self, from: data)
-                completion(.success(user))
-            } catch {
-                completion(.failure(DownloadError.invalidJson(error: String(describing: error))))
-            }
-        }
-        task.resume()
-    }
 }
